8f8b61f5202fd998115eb582e3013bf95e268992
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-01-26 14:13:40 -0500

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -18,38 +18,33 @@ class Producer(object):
                 "stoptime": {"index": 2, "type": "str"},
                 "start station":  {"index": 4, "type": "str"},
                 "end station":   {"index": 8, "type": "str"},
-                "bikeid":   {"index": 11, "type": "str"}
-            }
-        }
+                "bikeid":   {"index": 11, "type": "str"}}}
 
-    def map_schema(line, schema):
-    """
-    cleans the message msg, leaving only the fields given by schema, and casts the appropriate types
-    returns None if unable to parse
-    :type line  : str       message to parse
-    :type schema: dict      schema that contains the fields to filter
-    :rtype      : dict      message in the format {"field": value}
-    """
+    def map_schema(self,line, schema):
         try:
             msg = line.split(schema["DELIMITER"])
             msg = {key:eval("%s(\"%s\")" % (schema["FIELDS"][key]["type"],
-                                    msg[schema["FIELDS"][key]["index"]]))
-                        for key in schema["FIELDS"].keys()}
+                                        msg[schema["FIELDS"][key]["index"]]))
+                            for key in schema["FIELDS"].keys()}
         except:
             return
         return msg
 
+
     def producer_msgs(self):
         s3 = boto3.client('s3')
-        obj = s3.get_object(Bucket="citibikes-data-bucket", Key= ""data/201502-citibike-tripdata.csv"")
+        obj = s3.get_object(Bucket="citibikes-data-bucket", Key= "data/201502-citibike-tripdata.csv")
         text =  obj['Body'].read().decode('utf-8')
         #starts after the headers in the csv
         text = text.split("\n")[1:]
 
+
         for line in text:
            message = line.strip()
-           msg = map_schema(message, self.schema)
-           self.producer.send("kiosk", value =dumps(msg)) #, key=self.get_key(msg))
+           msg = self.map_schema(message, self.schema)
+           print("msg with schema: ", msg)
+           # topic set up already in command line
+           self.producer.send("kiosk", value =dumps(msg)) # ,key=self.get_key(msg))
            sleep(5)
 
 if __name__ == "__main__":
@@ -59,4 +54,3 @@ if __name__ == "__main__":
     # prod =Producer(ip_addr)
     prod = Producer("localhost:9092")
     prod.producer_msgs()
-
