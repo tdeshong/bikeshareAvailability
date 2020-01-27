1a24e0ee288b6f401ba881fc210159e3a07f260b
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-01-27 15:32:04 -0500

    Update stream.py
    process_stream()  updated

--- streaming/stream.py
+++ streaming/stream.py
@@ -15,29 +15,39 @@ class Streamer(object):
     #figure out how to use session with kafkautils
     #takes in a list of brokers
     def __init__(self, topic, broker):
-        self.sc = SparkContent().getOrCreate()
+        self.sc = SparkContext()
         self.ssc = StreamingContext(self.sc,2)
         self.sc.setLogLevel("ERROR")
-
-        self.stream= KafkaUtils.createDirectStream(self.ssc, "kiosk",\
-                                 {“metadata.broker.list”: broker})
+        #this does not work but in many examples
+        #broker = self._kafkaTestUtils.brokerAddress()
+        #print("brokers list: ", broke)
+        self.stream= KafkaUtils.createDirectStream(self.ssc, ["kiosk"],{"metadata.broker.list":",".join(broker)})
 
     # more processing will happen at a later date
     def process_stream(self):
-        self.initialize_stream()
-        convert = self.stream.map(lambda x: x[1])
-        convert = self.stream.map(lambda x: loads(x[1]))
-        test_output.pprint()
+        #self.initialize_stream()
+        self.stream.pprint()
+        #self.stream.foreachRDD(lambda rdd:rdd.foreachPartition(self.printstream))
+        #convert = self.stream.map(lambda x: x[1])
+        #convert = self.stream.map(lambda x: loads(x[1]))
+        #test_output.pprint()
+
+    def printstream(self, stream):
+        print(stream)
 
 
     def run(self):
         self.process_stream()
         self.ssc.start()
-        self.ssc.awaitTermination(timeout=180)
+        print("started spark context")
+        self.ssc.awaitTermination()
 
 
 if __name__ == "__main__":
     args = sys.argv
-    print ("args: ", args)
-    # bike = Streamer("kiosk", broker)
-    # bike.run()
+    print ("Streams args: ", args)
+    topic = "kiosk"
+    broker =["ip-10-0-0-9:9092","ip-10-0-0-11:9092", "ip-10-0-0-4:9092"]
+    # bikes = Streamer(topic)
+    bikes = Streamer(topic, broker)
+    bikes.run()
