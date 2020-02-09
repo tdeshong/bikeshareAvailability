f2485445284d73321aae3606096d1184ba3e7209
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-09 07:33:15 -0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import (Flask, url_for, flash, render_template, request, jsonify)
 import database
+import folium
 
 app = Flask(__name__)
 
@@ -12,8 +13,16 @@ def index():
     conn = database.getConn(db)
     all = database.sample(conn)
     #now = servertime.now()
+    folium_map = folium.Map(location=[40.738, -73.98],
+                        width='50%',
+                        zoom_start=13,
+                        tiles="CartoDB dark_matter")
+    folium.CircleMarker(location=[40.738, -73.98],fill=True).add_to(folium_map)
+    folium_map.save('templates/map.html')
+#    return folium_map._repr_html_()
     return render_template('index.html',  base=all)
 
 if __name__ == '__main__':
     app.debug = True
     app.run('0.0.0.0',5000)
+
