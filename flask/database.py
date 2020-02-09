57ed2686145d6bc4e610963768177dac89933b17
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-09 10:35:33 -0500

    Update database.py

--- flask/database.py
+++ flask/database.py
@@ -17,21 +17,27 @@ def sample(conn):
     curs.execute('select * from location limit 10')
     return curs.fetchall()
 
-def findNeighbors(conn, location):
+def findNeighbors(conn, time, location):
     #columns will be returned as a dictionary
-    curs = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    curs = conn.cursor()
+    latUpper = location[0]+0.007
+    latLower = location[0]-0.007
+    longUpper = location[1]+0.007
+    longLower = location[1]-0.007
     #write the query for the
-    curs.execute('select')
+    curs.execute('''Select distinct(c1.loc) from 
+                (select * from records a1 inner join 
+                (select bike_id, max(time) as max from records group by bike_id having max(time)<'{}')b1 
+                 on a1.bike_id = b1.bike_id and a1.time=b1.max)c1 where loc[0] between latUpper and latLower
+                 and loc[1] between longUpper and longLower'''.format(time))
     return curs.fetchall()
 
-def slots(conn):
-    curs = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-    #write the query for the
-    curs.execute('select')
+def slots(conn, time):
+    curs = conn.cursor()
+    curs.execute('''Select distinct(c1.loc) from 
+                (select * from records a1 inner join 
+                (select bike_id, max(time) as max from records group by bike_id having max(time)< '{}' )b1 
+                 on a1.bike_id = b1.bike_id and a1.time=b1.max)c1 where status = True'''.format(time))
     return curs.fetchall()
 
-def test(conn):
-    curs = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-    #write the query for the
-    curs.execute('select * from test2')
-    return curs.fetchall()
+
