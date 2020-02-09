eae455de4aa12bab449d2f281bd06e6748188cbf
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-09 10:36:21 -0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -4,27 +4,34 @@ import folium
 
 app = Flask(__name__)
 
-#change this
+
+#change this later
 db= 'bikeshare'
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        print(request.form["location"], type(request.form["location"]))
     conn = database.getConn(db)
-    all = database.sample(conn)
-    #now = servertime.now()
     folium_map = folium.Map(location=[40.738, -73.98],
                         width='50%',
                         zoom_start=13,
                         tiles="CartoDB dark_matter")
-    folium.CircleMarker(location=[40.738, -73.98],fill=True).add_to(folium_map)
+    if request.method == 'POST':
+       print("in the post")
+       input = request.form["location"].lower()
+       #prevent sql injection
+       errorWords =['select', 'delete', 'update','insert', 'create', 'drop table'] 
+       for word in errorWords:
+          if word in input:
+             flash("Improper Location")
+       #function to find it
+    else:
+        all = database.slots(conn, '2013-07-01 10:00:00')
+        print(all[0][0], len(all))
+        for points in all:
+           folium.CircleMarker(location=points[0],fill=True).add_to(folium_map)
     folium_map.save('templates/map.html')
-#    return folium_map._repr_html_()
+
     return render_template('index.html',  base=all)
-
-#def neighbors()
-
 
 if __name__ == '__main__':
     app.debug = True
