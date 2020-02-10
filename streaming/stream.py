7410c24a6479d124118ec7720e33c2936888f956
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-09 21:37:22 -0500

    Update stream.py

--- streaming/stream.py
+++ streaming/stream.py
@@ -37,98 +37,105 @@ class Streamer(object):
         #broker = self._kafkaTestUtils.brokerAddress()
         self.stream= KafkaUtils.createDirectStream(self.ssc, ["kiosk"],{"metadata.broker.list":",".join(broker)})
     
-    def ready (self, anRDD, something):
-       x = self.readyForDB(anRDD, something)
-       self.readyLoc(x)
-       self.readyRecords(x)
-    
-    def readyForDB(self, anRDD, something):
-            org = self.spark.createDataFrame(something, self.original)
+    def insert(self, time, anRDD):
+       '''
+       call function that creates dataframe from the rdd in the direct stream
+       call functions that will insert the information from the dataframe
+       into the appropriate tables in the database
+       '''
+       df = self.createDataframe(time, anRDD)
+       self.readyLoc(df)
+       self.readyRecords(df)
 
-            #casting columns to the appropriate type
-            org = org.withColumn("tripduration", org["tripduration"].cast(IntegerType()))
-            org = org.withColumn("bike_id", org["bike_id"].cast(IntegerType()))
-            org = org.withColumn("startLat", org["startLat"].cast(DoubleType()))
-            org = org.withColumn("startLong", org["startLong"].cast(DoubleType()))
-            org = org.withColumn("endLat", org["endLat"].cast(DoubleType()))
-            org = org.withColumn("endLong", org["endLong"].cast(DoubleType()))
+    def createDataframe(self, time, anRDD):
+        '''
+        creates dataframe with the information from the incoming stream
+        Adds a column which contains an array of the latitude and londitude
+        coordinates of the start location and another column which contains
+        an array of the latitude and londitude coordinates of the end location
 
-            #create extra columns with the lat and long combined in an array
-            #f is an alias for pyspark.sql.function
-            org = org.withColumn("latLongStart", f.array("startLat","startLong"))
-            org = org.withColumn("latLongEnd", f.array("endLat", "endLong"))
+        returns dataframe
+        '''
+        org = self.spark.createDataFrame(anRDD, self.original)
 
-            return org
+        #casting columns to the appropriate type
+        org = org.withColumn("tripduration", org["tripduration"].cast(IntegerType()))
+        org = org.withColumn("bike_id", org["bike_id"].cast(IntegerType()))
+        org = org.withColumn("startLat", org["startLat"].cast(DoubleType()))
+        org = org.withColumn("startLong", org["startLong"].cast(DoubleType()))
+        org = org.withColumn("endLat", org["endLat"].cast(DoubleType()))
+        org = org.withColumn("endLong", org["endLong"].cast(DoubleType()))
+
+        #create extra columns with the lat and long combined in an array
+        #f is an alias for pyspark.sql.function
+        org = org.withColumn("latLongStart", f.array("startLat","startLong"))
+        org = org.withColumn("latLongEnd", f.array("endLat", "endLong"))
+
+        return org
 
     def readyLoc(self, df):
-       #'''
-        #the location database contains many duplication of location bc the producer
-        #is duplicating the records .
-       #'''
+        '''
+        Parameter: dataframe with schema as the one returned by createDataFrame()
+        
+        creates dataframe with 2 columns: street name and an array with latitude and longtitude
+        inserts the dataframe into the location table in the database
+        '''
+
 
         try:
-            #creating new dataframe from the start and end locations 
+            #creating new dataframe from the start and end locations
             df1 =df.select("locationStart", "latLongStart")
             df1 = df1.withColumnRenamed("locationStart", "street").withColumnRenamed("latLongStart","coordinates")
 
             df2 =df.select("locationEnd", "latLongEnd")
             df2 = df2.withColumnRenamed("locationStart", "street").withColumnRenamed("latLongStart","coordinates")
             both = df1.union(df2)
-            #tried ignore for mode  and it did nothing to the database
+            
             both.write.jdbc(url=self.url, table='location', mode='append', properties=self.properties).save()
         except AttributeError:
             pass
+
+     def readyRecords(self, df):
+        '''
+        Parameter: dataframe with schema as the one returned by createDataFrame()
         
-     def readyRecords(self, df): 
+        creates dataframe with 4 columns: bike_id, timestamp, location in latitude and
+                                          longtitude, status of the bike as boolean
+        inserts the dataframe into the location table in the database
+        '''
         try:
            df1 = df.select("bike_id","Starttime", "latLongStart")
-#           print(df1.collect())
            df1 = df1.withColumnRenamed("Starttime", "time").withColumnRenamed("latLongStart","loc")
-#           print(df1.collect())
            df1 =df1.withColumn("status", f.lit(False))
-           print(df1.collect())
 
            df2 = df.select("bike_id","Stoptime", "latLongEnd")
            df2 = df2.withColumnRenamed("Stoptime", "time").withColumnRenamed("latLongEnd","coordinates")
            df2 =df2.withColumn("status", f.lit(True))
            both = df1.union(df2)
-           print(both.collect())
            both.write.jdbc(url=self.url, table='records', mode='append', properties=self.properties).save()
         except AttributeError:
             pass
-    
-     def write(self, table, timing, item):
-        ''' 
-        function not in use
-        returns boolean for if you can write to the database or not
-        '''
-        locExist = False
-        if table =='location':
-           #if throws an error put () outside the string
-           if locExist:
-               query = '(select * from location where "{}" = "{}") as foo'.format(timing,item)
-               loc =self.spark.read.jdbc(url=self.url, table = query, properties = self.properties)
-               loc.pprint()
-               print(loc)
-           #some conditional for if it does not have it
-           #make the data
 
- #wrote to database it says
-        #return df
 
-    # more processing will happen at a later date
-    def process_stream(self):
+
+    def processStream(self):
+       '''
+       Encodes the stream from the broker and
+       sends to insert() to insert the applicable information to the applicable
+       tables in the database
+       '''
        #no need to encode because python3 handles it
        convert = self.stream.map(lambda x: json.loads(x[1]))
        convert.pprint()
-       convert.foreachRDD(self.ready)
+       convert.foreachRDD(self.insert)
 
 
     def run(self):
-        self.process_stream()
+        self.processStream()
         self.ssc.start()
         print("started spark context")
         self.ssc.awaitTermination()
+
 
 
 if __name__ == "__main__":
