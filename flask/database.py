40a103f9209a2fe755267b4038badeb431752495
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-12 11:59:03 -0500

    Update database.py

--- flask/database.py
+++ flask/database.py
@@ -7,14 +7,8 @@ Database api that queries bike location from postgres database
 '''
 def getConn(db):
     conn = psycopg2.connect("host= ec2-18-210-209-145.compute-1.amazonaws.com dbname=bikeshare user=ubuntu password=123")
-#    conn.autocommit(True)
     return conn
 
-def sample(conn):
-    # curs = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-    curs = conn.cursor()
-    curs.execute('select * from location limit 10')
-    return curs.fetchall()
 
 def findNeighbors(conn, time, location):
     '''
@@ -28,7 +22,7 @@ def findNeighbors(conn, time, location):
     latLower = location[0]-0.007
     longUpper = location[1]+0.007
     longLower = location[1]-0.007
-    #write the query for the
+
     curs.execute('''Select distinct(c1.loc) from 
                 (select * from records a1 inner join 
                 (select bike_id, max(time) as max from records group by bike_id having max(time)<'{}')b1 
