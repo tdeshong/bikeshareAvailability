c8dbe0136e63e0df5af68d557a487320241ce008
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-02-17 20:22:13 -0500

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -8,39 +8,9 @@ class Producer(object):
     Creates a Kafka Producer object and sends rows of information from csv
     files in s3 bucket to Kafka consumer
     '''
-    def __init__(self, addr):
+    def __init__(self, addr, schema, fields):
         '''
         addr --> list of public ip addresses
-        '''
-        self.producer = KafkaProducer(bootstrap_servers =[addr], \
-                                  value_serializer = lambda x: dumps(x).encode('utf-8'))
-        
-        #debating if this should be arguments for the Prducer object or just extra information provided
-        #later for map_schema
-        self.fields = ['bike_id', 'starttime', 'stoptime', 'start station','startLat',
-                       'startLong', 'end station','endLat', 'endLong']
-        self.schema = {
-            "DELIMITER":  ",",
-            "FIELDS":
-            {
-                "starttime":   {"index": 1, "type": "str"},
-                "stoptime": {"index": 2, "type": "str"},
-                "start station":  {"index": 4, "type": "str"},
-                "startLat" :{"index":5, "type": "str"},
-                "startLong": {"index":6,  "type": "str"},
-                "endLat": {"index":9,  "type": "str"},
-                "endLong":{"index":10, "type": "str"},
-                "end station":   {"index": 8, "type": "str"},
-                "bike_id":   {"index": 11, "type": "int"}}}
-
-    def map_schema(self,line, schema, fields):
-        '''
-        Parameters: line   --> row of csv as a string
-                    schema --> see __init__ for details
-                    fields --> see __init__ for details
-
-        Returns: list of desired columns from row
-
         schema --> dictionary with schema
                      { "DELIMITER": delimiter as str
                        "FIELDS"   : {"column name1": {"INDEX": number as int}
@@ -48,15 +18,27 @@ class Producer(object):
                                      ...
                                     }
                      }
-        field --> list of field keys in the desired order that the information
+        fields --> list of field keys in the desired order that the information
                   should be sent to the consumer
         '''
+        self.producer = KafkaProducer(bootstrap_servers =[addr], \
+                                  value_serializer = lambda x: dumps(x).encode('utf-8'))
+        
+        self.schema = schema
+        self.fields = fields
+        
+
+    def map_schema(self,line):
+        '''
+        Parameters: line   --> row of csv as a string
+        Returns: list of desired columns from row
+        '''
         try:
-            msg = line.split(schema["DELIMITER"])
+            msg = line.split(self.schema["DELIMITER"])
             data =[]
-            for key in fields:
-               datatype = schema["FIELDS"][key]["type"]
-               dataIndex = schema["FIELDS"][key]["index"]
+            for key in self.fields:
+               datatype = self.schema["FIELDS"][key]["type"]
+               dataIndex = self.schema["FIELDS"][key]["index"]
                #strip items of extra quotations
                info = msg[dataIndex].strip('"')
                data.append(info)
@@ -87,14 +69,32 @@ class Producer(object):
             for line in text:
                message = line.strip()
                msg = self.map_schema(message, self.schema,self.fields)
+               # split the row into 2 different messages
+               msgStart = [msg[0], msg[1], msg[3], msg[4], msg[5]]
+               msgEnd = [msg[0], msg[2], msg[6], msg[7], msg[8]]
                msgKey = name[1].encode('utf-8')
-               self.producer.send(topic, value=dumps(msg), key =msgKey)
-               sleep(5)
+               
+               self.producer.send(topic, value=dumps(msgStart), key =msgKey)
+               self.producer.send(topic, value=dumps(msgEnd), key =msgKey)
+
 
 if __name__ == "__main__":
     args = sys.argv
-    print("sys arg values: ", sys.argv)
-    ip_addr = str(args[1])
+    fields = ['bike_id', 'starttime', 'stoptime', 'start station','startLat','startLong', 'end station','endLat', 'endLong']
+    schema = {
+            "DELIMITER":  ",",
+            "FIELDS":
+            {
+                "starttime":   {"index": 1, "type": "str"},
+                "stoptime": {"index": 2, "type": "str"},
+                "start station":  {"index": 4, "type": "str"},
+                "startLat" :{"index":5, "type": "str"},
+                "startLong": {"index":6,  "type": "str"},
+                "endLat": {"index":9,  "type": "str"},
+                "endLong":{"index":10, "type": "str"},
+                "end station":   {"index": 8, "type": "str"},
+                "bike_id":   {"index": 11, "type": "int"}}}
+    
     addr = "put in secrets the 1 broker"
     prod =Producer(addr)
     prod.producer_msgs()
